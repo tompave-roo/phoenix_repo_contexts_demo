aaede7821b1749b241ff026954252bedd7d80f57
Tommaso Pavese <tommaso.pavese@deliveroo.co.uk>
2018-10-30 14:31:22 +0000

    mix phx.gen.json Accounts User users name:string age:integer

---
+++ lib/alpaca_web/controllers/fallback_controller.ex
@@ -0,0 +1,22 @@
+defmodule AlpacaWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use AlpacaWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(AlpacaWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(AlpacaWeb.ErrorView)
+    |> render(:"404")
+  end
+end
